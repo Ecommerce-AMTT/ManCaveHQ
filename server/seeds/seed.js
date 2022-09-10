616c52439f1393bd1d84a9a3f3a9f9e561dcce48
Tommy Lee <t.k.hobbes43@gmail.com>
2022-09-10 14:17:58 -0500

    still can't get it to seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,12 +2,15 @@ const db = require("../config/connection");
 const { User, Product, Category } = require("../models");
 
 const productData = require("./productData.json");
+const categoryData = require("./categoryData.json");
 
 db.once("open", async () => {
     await Product.deleteMany({});
 
+    const category = await Category.insertMany(categoryData);
     const products = await Product.insertMany(productData);
     
+    console.log("categories seeded!", category);
     console.log("products seeded!", products);
 
     process.exit(0);
