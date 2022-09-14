7c01294639d0796a387cd1a0e26dea0a64a3fd49
Tommy Lee <t.k.hobbes43@gmail.com>
2022-09-14 18:14:12 -0500

    forgot to comment out one more thing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import {Container} from 'react-bootstrap'
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-import SearchBar from "../components/SearchBar";
+// import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   return (
